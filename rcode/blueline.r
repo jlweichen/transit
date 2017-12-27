d393968b9fdf2c7883b73b999825daca9d171309
jlweichen <32230988+jlweichen@users.noreply.github.com>
2017-12-26 19:15:27 -0500

    Create blueline.r
    Code to clean data in R for Blue Line

---
+++ rcode/blueline.r
@@ -0,0 +1,30 @@
+library("readr")
+library("maptools")
+library("dplyr")
+library("tibble")
+
+# https://data.cityofchicago.org/
+# reading CTA data from downloaded CSV files
+dayBoarding <- read_csv(#path to .csv file#)
+stops <- read_csv(#path to .csv file#)
+# right now, I am only interested in the blue line
+blueStops <- subset(stops, BLUE=="true")
+remove(stops)
+blues <- c(unique(blueStops$MAP_ID))
+blueLatLong <- c(unique(blueStops$Location))
+# I also am only interested in the year 2016 and newer ridership data
+dayBoarding[,3] <- lapply(dayBoarding[,3], as.Date, format="%m/%d/%Y")
+dayBoarding <- filter(dayBoarding, date > "2015-12-31")
+
+
+#empty tibble for blue line ridership data
+blueRiders <- tibble()
+# looping through the table and saving blue line data to blueFrame
+for(i in 1:length(blues)){
+  for(j in 1:length(dayBoarding$station_id)){
+    if(as.integer(dayBoarding$station_id[j]) == blues[i]){
+      blueRiders <- bind_rows(blueRiders, dayBoarding[j,])
+    }
+  }
+}
+remove(dayBoarding)
