5c74badecb1969a6b6456a2d755bf7e8eb2b6858
jlweichen <32230988+jlweichen@users.noreply.github.com>
2017-12-27 15:55:49 -0500

    Update blueline.r

--- rcode/blueline.r
+++ rcode/blueline.r
@@ -4,16 +4,21 @@ library("tibble")
 
 # https://data.cityofchicago.org/
 # reading CTA data from downloaded CSV files
-dayBoarding <- read_csv(#path to CSV file#)
-stops <- read_csv(#path to CSV file#)
-# right now, I am only interested in the blue line
+dayBoarding <- read_csv(#path to CSV#)
+stops <- read_csv(#path to CSV#)
+# right now, I am only interested in the blue line, and relevant columns
 blueStops <- subset(stops[,c(5,6,9,17)], BLUE=="true")
 blueStops <- subset(blueStops[,c(1,2,4)])
 
 remove(stops)
+
+#vectors containing the names and IDs of Blue Line stops
+
 blues <- c(unique(blueStops$MAP_ID))
 blueNames <- c(unique(blueStops$STATION_DESCRIPTIVE_NAME))
-blueNames <- (c(blueNames[(1:4)], blueNames[6:34]))
+# California stop name is duplicated with a typo in original data
+# so we remove one of those entries
+blueNames <- c(blueNames[(1:4)], blueNames[6:34])
 
 
 # I also am only interested in the year 2016 and newer ridership data
@@ -31,11 +36,11 @@ for(i in 1:length(blues)){
     }
   }
 }
-
-remove(dayBoarding)
+remove(dayBoarding, i, j)
 blueRiders <- arrange(blueRiders, date)
 
 # splitting the coordinates string into latitude and longitude
+# using regex and looping through
 blueLatLong <- c(unique(blueStops$Location))
 
 blueCoords <- tibble(lat=1:length(blueNames), long=1:length(blueNames))
@@ -45,27 +50,26 @@ for(i in 1:length(blueNames)){
   blueCoords$lat[i] <- regmatches(blueLatLong[i], regexpr(latReg, blueLatLong[i]))
   blueCoords$long[i] <- regmatches(blueLatLong[i], regexpr(longReg, blueLatLong[i]))
 }
+remove(i)
 
 blueStops <- tibble(name = blueNames, MAP_ID = blues)
 blueStops <- bind_cols(blueStops, blueCoords)
 
 remove(blueCoords)
 
-write_csv(blueRiders, #path to save CSV#)
-write_csv(blueStops, #path to save CSV#)
+#writing CSV files containing stops information
+write_csv(blueStops, "#path#blueStops.csv")
 
 #now making a frame for each station's ridership data
 
-riders1 <- subset(blueRiders, station_id == 41240)
-riders2 <- subset(blueRiders, station_id==blueStops$MAP_ID[2])
-
 for(i in 1:length(blueStops$name)){
   riders <- subset(blueRiders, station_id==blueStops$MAP_ID[i])
-  # right now not using the meta-data but I'm saving it to CSV in case I want to in the future
-  write_csv(riders, paste(#directory path#"/riders",as.character(i), ".csv"))
+  # exporting each stop to its own CSV
+  write_csv(riders, paste(#path#"/riders",as.character(i), ".csv"))
   assign(paste("riders",as.character(i)), riders)
   remove(riders)
 }
+remove(i)
 
 #creating r data file
-save.image(#directory path#".RData")
+save.image(#path#"/ridership.RData")
