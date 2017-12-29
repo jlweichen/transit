be098ebe6de3caceb0530fc8e1c027cfa1ee408d
jlweichen <32230988+jlweichen@users.noreply.github.com>
2017-12-28 22:06:41 -0500

    Update pullingTweets.r

--- rcode/pullingTweets.r
+++ rcode/pullingTweets.r
@@ -1,22 +1,23 @@
 library("twitteR")
 library("dplyr")
 library("readr")
+library("tidyr")
 
 # used https://opensource.com/article/17/6/collecting-and-mapping-twitter-data-using-r
 # helpful guide with twitteR library
 # and http://thoughtfaucet.com/search-twitter-by-location/examples/
 
-APIkey <- #API key
-APIsecret <- #API secret
-tokenKey <- #token key
-tokenSecret <-#token secret
+APIkey <- # API key
+APIsecret <- # API secret
+tokenKey <- # token key
+tokenSecret <- # token secret
 
 # Chicago woeid = 2379574 used when finding what's trending locally
 
 setup_twitter_oauth(APIkey, APIsecret, tokenKey, tokenSecret)
 
 # loading the Blue Line data
-load(#path to ridership.RData)
+load("~/Documents/chicago/data/ridership.RData")
 
 # now to make a list with the total number of tweets for each stop
 totalTweets <-c()
@@ -39,32 +40,24 @@ for(i in 1:length(blueNames)){
   #stopFrame <- filter(stopFrame, is.na(stopFrame$latitude) == FALSE)
   bigTweets <- bind_rows(bigTweets, stopFrame)
   # if I want to archive the tweets in CSV I uncomment the below and edit file name
-  # write_csv(stopFrame, paste(#path#"frame",as.character(i), ".csv", sep=""))
+  write_csv(stopFrame, paste("~/Documents/chicago/data/122817frame",as.character(i), ".csv", sep=""))
   totalTweets[i] <- length(stopFrame$id)
-  Sys.sleep(10)
+  Sys.sleep(25)
 }
 remove(i, stopFrame, stopTweets, search)
-# we will need the lat and long later, so making numeric now
-bigTweets$latitude <- as.numeric(bigTweets$latitude)
-bigTweets$longitude <- as.numeric(bigTweets$longitude)
-
-# want to remove tweets with NA for long or lat
-cleanTweets <- data_frame()
-
-for(i in 1:length(bigTweets$id)){
-  if(is.na(bigTweets$latitude[i])==FALSE & is.na(bigTweets$longitude[i])==FALSE){
-    columnar <- bigTweets[i,]
-    cleanTweets <- bind_rows(cleanTweets, columnar)
-  }
-}
-remove(bigTweets, columnar)
 
 #removing duplicate tweets
-cleanTweets <- cleanTweets[!duplicated(cleanTweets), ]
+bigTweets <- unique(bigTweets)
 
+# removing tweets without geodata
+cleanTweets <- drop_na(bigTweets)
+
+# ensuring the lat and long columns are numeric
+cleanTweets$latitude <- as.numeric(cleanTweets$latitude)
+cleanTweets$longitude <- as.numeric(cleanTweets$longitude)
 #double check the size of cleanTweets and tweetIDs are the same size
 #could use logcial check but I use browser which shows they are both the same size
 tweetIDs <- c(unique(cleanTweets$id))
 
-remove(i, APIsecret, APIkey, tokenSecret, tokenKey, tweetIDs)
-save.image(#path to save to, "twitter.RData")
+remove(APIsecret, APIkey, tokenSecret, tokenKey, bigTweets)
+save.image("~/Documents/chicago/data/twitter.RData")
