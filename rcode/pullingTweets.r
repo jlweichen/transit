2c40831c5c815a5ed8adfd260363aba217691ef8
Jennifer <32230988+jlweichen@users.noreply.github.com>
2018-05-06 16:17:27 -0400

    Update pullingTweets.r
    Changed library used to rtweet, made some changes to make things smoother

--- rcode/pullingTweets.r
+++ rcode/pullingTweets.r
@@ -1,63 +1,76 @@
-library("twitteR")
+library("rtweet")
 library("dplyr")
 library("readr")
 library("tidyr")
 
-# used https://opensource.com/article/17/6/collecting-and-mapping-twitter-data-using-r
-# helpful guide with twitteR library
-# and http://thoughtfaucet.com/search-twitter-by-location/examples/
+#using rtweet library and Oauth2.0
 
-APIkey <- # API key
-APIsecret <- # API secret
-tokenKey <- # token key
-tokenSecret <- # token secret
+appname <- #the app name you selected on app.twitter.com
+key <- # API key
+secret <- # API secret
 
-# Chicago woeid = 2379574 used when finding what's trending locally
+twitter_token <- create_token(
+  app = appname,
+  consumer_key = key,
+  consumer_secret = secret)
 
-setup_twitter_oauth(APIkey, APIsecret, tokenKey, tokenSecret)
+# making a vector of base lat and long coords, created in QuantumGIS
+# these cover the city of Chicago in a grid
+allpoints <- read_csv("~/chicago/allpoints.csv")
+for(i in 1:length(allpoints)){
+  allpoints$LAT[i] <- as.numeric(allpoints$LAT[i])
+  allpoints$LONG[i] <- as.numeric(allpoints$LONG[i])
+}
 
-# loading the Blue Line data
-load("~/Documents/chicago/data/ridership.RData")
-
-# now to make a list with the total number of tweets for each stop
+# now to make a list with the total number of tweets around each point
 totalTweets <-c()
 
 #empty tibble for all tweets returned by API query
 bigTweets <- as_data_frame()
 
-# with this for loop, we repeat the procedure for each of the 33 stops
-# radius of half mile chosen; walking distance, allows good number of tweets
-# smaller radius gives hyper-localized tweets, n of 1000 tweets max per stop
-# tweets since Dec. 15, 2017, but will likely be more recent per API limit to last 7 days
-# aware of overlap near close-together stations such as those in the Loop
+# with this for loop, we repeat the procedure for each of the points selected
+# radius of 1.4 miles chosen; blanketing the area, allows good number of tweets
+# smaller radius and more points - more tweets!
+# sadly, also means more calls to the API and if we make too many, the API times out
+# we space the queries 25 seconds apart for this reason
+# n of 1800 tweets max per query
+# tweets over the past one week, or last 604800 seconds of time!
+# aware of overlap possibility and will filter
+# duplicate posts later
+if(dir.exists("~/chicago/data")==FALSE){
+    dir.create("~/chicago/data")
+}
+trendPath <- paste("~/chicago/data/tweets/", format(Sys.time(), format="%Y%m%d"), sep="")
+if(dir.exists(trendPath)==FALSE){
+    dir.create(trendPath)
+}
 
-for(i in 1:length(blueNames)){
-  search <- paste("geocode:", as.character(blueStops$lat[i]), ",",sep="")
-  search <- paste(search, as.character(blueStops$long[i]),",0.5mi", sep="")
-  stopTweets <- searchTwitter(search, since = "2017-12-15",n = 1000, lang = "en")
-  stopFrame <- twListToDF(stopTweets)
-  stopFrame <- subset(stopFrame[,c(1,5,8,10,11,15,16)])
-  #stopFrame <- filter(stopFrame, is.na(stopFrame$latitude) == FALSE)
-  bigTweets <- bind_rows(bigTweets, stopFrame)
+for(i in 1:length(allpoints$LAT)){
+  search <- paste("geocode:", as.character(allpoints$LAT[i]), ",",sep="")
+  search <- paste(search, as.character(allpoints$LONG[i]),",1.4mi", sep="")
+  stopTweets <- search_tweets(search, since = format(Sys.time()-604800, format="%Y-%m-%d"),n = 1800, lang = "en")
+  if(length(stopTweets) == 0){
+    next
+  }
+  bigTweets <- bind_rows(bigTweets, stopTweets)
   # if I want to archive the tweets in CSV I uncomment the below and edit file name
-  write_csv(stopFrame, paste("~/Documents/chicago/data/122817frame",as.character(i), ".csv", sep=""))
-  totalTweets[i] <- length(stopFrame$id)
-  Sys.sleep(25)
+  write_as_csv(stopTweets, paste(trendPath,as.character(i), ".csv", sep=""))
+  totalTweets[i] <- length(stopTweets$id)
+  remove(stopTweets)
+  Sys.sleep(35)
 }
 remove(i, stopFrame, stopTweets, search)
 
 #removing duplicate tweets
 bigTweets <- unique(bigTweets)
 
-# removing tweets without geodata
-cleanTweets <- drop_na(bigTweets)
-
 # ensuring the lat and long columns are numeric
-cleanTweets$latitude <- as.numeric(cleanTweets$latitude)
-cleanTweets$longitude <- as.numeric(cleanTweets$longitude)
-#double check the size of cleanTweets and tweetIDs are the same size
-#could use logcial check but I use browser which shows they are both the same size
-tweetIDs <- c(unique(cleanTweets$id))
+bigTweets$latitude <- as.numeric(bigTweets$latitude)
+bigTweets$longitude <- as.numeric(bigTweets$longitude)
 
-remove(APIsecret, APIkey, tokenSecret, tokenKey, bigTweets)
-save.image("~/Documents/chicago/data/twitter.RData")
+
+remove(APIsecret, APIkey, tokenSecret, tokenKey)
+save.image("~/chicago/data/twitter.RData")
+
+# if I want to archive bigTweets I write it as a separate CSV
+write_csv(bigTweets, paste(trendPath, "/bigTweets", format(Sys.time(), format="%Y%m%d"), ".csv", sep=""))
